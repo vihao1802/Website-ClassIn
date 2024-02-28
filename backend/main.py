40f011edff6276be40ca80d953c974c79a9ed494
Nameless-h <tranvihao40@gmail.com>
2024-02-28 22:34:59 +0700

    feat: Initial project setup

---
+++ backend/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from hume import HumeStreamClient
+from hume.models.config import LanguageConfig
+from collections import defaultdict
+import os
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from typing import List
+from dotenv import load_dotenv
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+async def hello():
+    return {"message": "Hello world"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
---
+++ backend/models.py
@@ -0,0 +1,4 @@
+from sqlalchemy import Boolean, Column, Integer, String
+from database import Base
+
+
